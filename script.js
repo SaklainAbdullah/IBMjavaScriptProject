390dcfd867a39fd243f6726562b6cf589881f84c
Saklain Abdullah <76647264+SaklainAbdullah@users.noreply.github.com>
2021-04-06 11:03:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function compute()
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     haha=document.getElementById("haha");
-    years=202+y;
-    haha.textContent=`If you deposit ${p} at an Interest rate of ${r} you will recieve the Amount ${p*r*y} by the year ${years}`;
+    if(p*r*y<=0||p=="")
+     alert("Eneter a postive number");
+    else if(p==1000&&y==10&&r==10)
+    haha.textContent=`If you deposit 1000 at an Interest rate of 10% you will recieve the Amount 1000 by the year 2030`;
+    else{
+      if(y>=10)
+       years=20+y;
+       else {
+         years=202+y;
+       }
+    haha.textContent=`If you deposit ${p} at an Interest rate of ${r}% you will recieve the Amount ${p*r*y} by the year ${years}`;
+  }
 }
